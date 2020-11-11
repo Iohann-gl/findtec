93ba90b2bb0e3aacb79a60d8fdcf3ce3b0d76ad0
Iohann-gl <find.tec02@gmail.com>
2020-11-11 18:30:55 -0300

    adicionando layout para cadastro de tecnico

---
+++ app/src/main/java/com/iohann/findtec/CadastroTecnico.java
@@ -0,0 +1,14 @@
+package com.iohann.findtec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroTecnico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_tecnico);
+    }
+}
